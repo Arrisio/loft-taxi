e8b6eaace4da25143f0d981b4b1ff08bfed54d14
Arrisio <SignumMea@gh1>
2020-04-14 10:15:45 +0300

    refactor: redesign tests

--- src/app/index.test.js
+++ src/app/index.test.js
@@ -2,43 +2,52 @@ import React, {createContext} from "react";
 import App from "./index";
 import {render, screen, fireEvent} from "@testing-library/react";
 
+const initTestQueries = () => {
+    const {getByTestId, queryByTestId } = render(<App/>);
+    return [getByTestId, queryByTestId]
+}
 
-describe("Ent2end", () => {
+describe("intro page", () => {
+    let getByTestId, queryByTestId;
+    beforeEach(() => {
+        [getByTestId, queryByTestId] = initTestQueries()
+    })
 
-    it("Инициализирована страница входа", () => {
-        const {getByTestId, queryByTestId, getByText} = render(<App/>);
+    it("renders without crashing", () => {
         expect(queryByTestId("mainPage")).toBeFalsy();
         expect(getByTestId("introPage")).toBeTruthy();
         expect(getByTestId("formSignIn")).toBeTruthy();
         expect(queryByTestId("formSignUp")).toBeFalsy();
     })
 
-    it("Переход на страницу регистрации", () => {
-        const {getByTestId, queryByTestId, getByText} = render(<App/>);
+    it("can toggle to signup form", () => {
         fireEvent.click(getByTestId("linkGotoSignUp"));
         expect(queryByTestId("formSignIn")).toBeFalsy();
         expect(getByTestId("formSignUp")).toBeTruthy();
     })
 
-    it("Переход на страницу Входа", () => {
-        const {getByTestId, queryByTestId, getByText} = render(<App/>);
+    it("can toggle to signin form", () => {
         fireEvent.click(getByTestId("linkGotoSignUp"));
 
         fireEvent.click(getByTestId("linkGotoSignIn"));
         expect(getByTestId("formSignIn")).toBeTruthy();
         expect(queryByTestId("formSignUp")).toBeFalsy();
     })
+});
 
-    it("Логинимся", () => {
-        const {getByTestId, queryByTestId, getAllByText} = render(<App/>);
-        fireEvent.submit(getByTestId('formSignIn'), {target: {username: 'a', password: '33'}});
+describe("auth", () => {
+    let getByTestId, queryByTestId;
+    beforeEach(() => {
+        [getByTestId, queryByTestId] = initTestQueries();
+        fireEvent.submit(getByTestId('formSignIn'), { target: { username: 'a', password: '33' } });
+    })
+
+    it("signin", () => {
         expect(queryByTestId("introPage")).toBeFalsy();
         expect(getByTestId("mainPage")).toBeTruthy();
     })
 
-    it("вылогиниваемся", () => {
-        const {getByTestId, queryByTestId, getAllByText} = render(<App/>);
-        fireEvent.submit(getByTestId('formSignIn'), {target: {username: 'a', password: '33'}});
+    it("signou", () => {
         fireEvent.click(getByTestId("btnSignOut"));
         expect(queryByTestId("introPage")).toBeTruthy();
         expect(queryByTestId("mainPage")).toBeFalsy();
