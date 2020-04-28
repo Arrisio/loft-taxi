b5c95d7055ce70fb728fadef1ac9d436de102bfc
Arrisio <SignumMea@gh1>
2020-04-28 22:22:29 +0300

    add saved card confirmation

--- src/layouts/map/order/order-form.test.js
+++ src/layouts/map/order/order-form.test.js
@@ -6,26 +6,24 @@ import '@testing-library/jest-dom/extend-expect'
 
 import {rootReducer} from '../../../modules'
 
-import { createStore } from 'redux'
+import {createStore} from 'redux'
 
 import {OrderForm} from "./order-form";
 import {renderWithProviders} from '../../../utils/test-utils'
 
 
-
-
 const store = createStore(rootReducer, {})
 describe("order form ", () => {
-        beforeEach(() => {
+    beforeEach(() => {
         jest.resetAllMocks();
     });
 
-    it("renders correctly",  () => {
-        const {getByTestId, queryByTestId, getByText} =  render(<OrderForm
+    it("renders correctly", () => {
+        const {getByTestId, queryByTestId, getByText} = render(<OrderForm
             classes={{}}
             fetchRoute={jest.fn()}
-            addresses ={[]}
-            confirmHandler = {jest.fn()}
+            addresses={[]}
+            confirmHandler={jest.fn()}
         />);
         expect(getByTestId("order-form")).toBeTruthy();
         expect(getByText("Откуда")).toBeTruthy();
@@ -34,46 +32,59 @@ describe("order form ", () => {
     })
 
     it("order sended successfully", async () => {
-    const confirmHandler=jest.fn();
+        const confirmHandler = jest.fn(() => {
+        });
 
-    const {getByTestId, queryByTestId, getByText} =  render(<OrderForm
-        classes={{}}
-        fetchRoute={jest.fn()}
-        addresses ={['addr1','addr2', 'addr3']}
-        confirmHandler = {confirmHandler}
-    />);
-    const fromAddrField = getByText("Откуда");
-    const toAddrField = getByText("Куда");
-    const makeOrderBtn = getByTestId("order-submit");
+        const {getByTestId, queryByTestId, getByText} = render(<OrderForm
+            classes={{}}
+            fetchRoute={jest.fn()}
+            addresses={['addr1', 'addr2', 'addr3']}
+            confirmHandler={confirmHandler}
+        />);
+        const fromAddrField = getByText("Откуда");
+        const toAddrField = getByText("Куда");
+        const makeOrderBtn = getByTestId("order-submit");
 
-
-
-    // wait(() => {fireEvent.submit(getByTestId('order-form'), {target: {address_from: 'addr1', address_to: 'addr2'}})});
-    // expect(confirmHandler).toHaveBeenCalled()
-
-
-    await selectEvent.select(fromAddrField, 'addr1');
-    expect(getByTestId("order-form")).toHaveFormValues({ address_from: 'addr1' });
-
-    await selectEvent.select(toAddrField, 'addr2');
-    expect(getByTestId("order-form")).toHaveFormValues({ address_to: 'addr2' })
-
-    wait (() => {
-        fireEvent.click(makeOrderBtn)
-    });
-    expect(confirmHandler).toHaveBeenCalled();
+        // wait(() => {fireEvent.submit(getByTestId('order-form'), {target: {address_from: 'addr1', address_to: 'addr2'}})});
+        // expect(confirmHandler).toHaveBeenCalled()
 
 
+        await selectEvent.select(fromAddrField, 'addr1');
+        expect(getByTestId("order-form")).toHaveFormValues({address_from: 'addr1'});
 
-    // await selectEvent.select(toAddrField, 'addr2')
-    // fireEvent.change(fromAddrField, { target: { label:'addr1',  value: 'addr1' } });
-    // expect(fromAddrField).toBe('addr1');
-    // fireEvent.change(toAddrField, { target: { label:'addr2',  value: 'addr2' } });
-    // expect(toAddrField).toBe('addr2');
+        await selectEvent.select(toAddrField, 'addr2');
+        expect(getByTestId("order-form")).toHaveFormValues({address_to: 'addr2'})
 
-    // fireEvent.click(makeOrderBtn);
-    // expectSaga
-    // wait(() => {fireEvent.submit(getByText('formSignIn'), {target: {username: 'test@df', password: '123'}})});
-})
+        wait(() => {
+            fireEvent.click(makeOrderBtn)
+        });
+        wait(() => {
+            expect(confirmHandler).toHaveBeenCalled()
+        });
+    })
+    it("order not sended without addresses", async () => {
+        const confirmHandler = jest.fn(() => {
+        });
 
+        const {getByTestId, queryByTestId, getByText} = render(<OrderForm
+            classes={{}}
+            fetchRoute={jest.fn()}
+            addresses={['addr1', 'addr2', 'addr3']}
+            confirmHandler={confirmHandler}
+        />);
+        const fromAddrField = getByText("Откуда");
+        const toAddrField = getByText("Куда");
+        const makeOrderBtn = getByTestId("order-submit");
+
+
+        await selectEvent.select(toAddrField, 'addr2');
+        expect(getByTestId("order-form")).toHaveFormValues({address_to: 'addr2'})
+
+        wait(() => {
+            fireEvent.click(makeOrderBtn)
+        });
+        wait(() => {
+            expect(confirmHandler).not.toHaveBeenCalled()
+        });
+    })
 });
