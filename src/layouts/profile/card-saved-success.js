ef9422b942ea9c55777d9facac973cd9124c6d82
Arrisio <SignumMea@gh1>
2020-04-29 09:25:53 +0300

    add saved card confirmation

--- src/layouts/profile/card-saved-success.js
+++ src/layouts/profile/card-saved-success.js
@@ -1,15 +1,12 @@
-import React, { useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import { connect } from 'react-redux';
+import React  from "react";
+import {Link, Link as RouterLink} from "react-router-dom";
 import withStyles from '@material-ui/core/styles/withStyles';
 import {Grid, Paper, Box, Typography, Button , Card} from "@material-ui/core/";
 
 import styles from './styles'
 
 
-const CardSavedSuccess = ({classes} ) => {
-    const history = useHistory();
-    return (
+const CardSavedSuccess = ({classes} ) =>  (
     <Paper className={classes.paper}>
         <Card className={classes.card} elevation={3}>
         <Grid container direction="column">
@@ -25,14 +22,15 @@ const CardSavedSuccess = ({classes} ) => {
                 variant="contained"
                 color="primary"
                 fullWidth
-                onClick={() => {history.push('/map')}}
+                component={Link}
+                to="/map"
             >
                 Сделать новый заказ
             </Button>
         </Grid>
         </Card>
     </Paper>
-)};
+);
 
 // export default connect(null, {clearRoute})(withStyles(styles)(CardSavedSuccess));
 export default withStyles(styles)(CardSavedSuccess);
