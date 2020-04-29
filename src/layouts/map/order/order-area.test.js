2beed3f0732bc0f7546297e35e3594c8b2045583
Arrisio <SignumMea@gh1>
2020-04-29 13:13:55 +0300

    fix: fix test desctiption

--- src/layouts/map/order/order-area.test.js
+++ src/layouts/map/order/order-area.test.js
@@ -18,27 +18,21 @@ import {renderWithProviders} from '../../../utils/test-utils'
 const store = createStore(rootReducer, {})
 describe("form area", () => {
     // jest.mock('./OrderForm', () => () => (<div>Hello World</div>));
-    // jest.mock('OrderForm', () => () => (<div>Hello World</div>));
-    // jest.mock(OrderForm, () => () => (<div>Hello World</div>));
-    // jest.mock('./need-card', () => () => (<div>Hello World</div>));
-    // jest.mock('./order-success', () => () => (<div>Hello World</div>));
     // OrderForm.mockImplementation(()=> <div>hello world</div>);
-    // beforeEach(() => {
-    //     jest.resetAllMocks();
-    // });
+
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
 
     it("renders need payment if no card",  () => {
         const {getByTestId, queryByTestId, getByText} =  renderWithProviders(
             <OrderArea/> , 
             createStore(rootReducer, {card:{isPaymentMethodReceived: false}})
         );
-        // const {getByTestId, queryByTestId, getByText} =  render(<MemoryRouter><OrderArea isPaymentMethodReceived={true} classes={{}}/></MemoryRouter>);
         expect(getByText("Заполните платежные данные")).toBeTruthy();
     })
 
     it("renders order form if card received",  () => {
-        // const {getByTestId, queryByTestId, getByText} =  renderWithProviders(<OrderArea isPaymentMethodReceived={true}/>);
-        // const {getByTestId, queryByTestId, getByText} =  render(<OrderArea isPaymentMethodReceived={true}/>);
         const {getByTestId, queryByTestId, getByText} =  renderWithProviders(
             <OrderArea/> ,
             createStore(rootReducer, {card:{isPaymentMethodReceived: true}})
